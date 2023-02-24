a9140716c2bf916e6bc622837683dc17ede253cf
AaronJesseManana <aaronjesse028@gmail.com>
2023-02-24 12:04:55 -0800

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,21 +3,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- * Return: Always 0 (success)
+ * positive_or_negative - test for negative or positive, or zero
+ * @i: the input number
+ * Return: Always 0 (succuss)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
 	else
-		printf("%d is negative\n", n);
-	return (0);
+		printf("%d is negative\n", i);
 }
